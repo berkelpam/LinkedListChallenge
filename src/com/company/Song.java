a34641272c10a3fe47649687b6b3db0a4b637af2
Paul Java <pauljava@Pauls-MacBook-Pro.local>
2017-08-10 09:28:16 +0200

    version 0.9 added additional overide toString for Song.

--- src/com/company/Song.java
+++ src/com/company/Song.java
@@ -19,4 +19,9 @@ public class Song {
     public double getDuration() {
         return duration;
     }
+
+    @Override
+    public String toString() {
+        return this.title + ":"+this.duration;
+    }
 }
